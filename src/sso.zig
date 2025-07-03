0642750ed37fea0a7fdfc69cf605fd045048e771
ProfessorLagg <futurecake2999@gmail.com>
2025-07-03 15:56:21 +0200

    sso string now FULLY works

--- src/sso.zig
+++ src/sso.zig
@@ -7,42 +7,34 @@ const maxStackLen: comptime_int = structSize - @sizeOf(u8);
 const maxStackBitLen: comptime_int = structBitSize - @bitSizeOf(u8);
 
 const StackString = packed struct {
-    const TVec: type = @Vector(maxStackLen, u8);
     const TUInt: type = std.meta.Int(.unsigned, maxStackBitLen);
     len: u8 = 0,
-    // val: TVec = @splat(0),
     val: TUInt = 0,
 
-    pub fn toSliceC(self: *const StackString) []const u8 {
-        //return @as([maxStackLen]u8, self.val)[0..self.len];
+    inline fn toSliceC(self: *const StackString) []const u8 {
         return b: {
             var r: []const u8 = undefined;
             r.len = self.len;
-            // r.ptr = @ptrFromInt(@intFromPtr(&self.val));
             r.ptr = @ptrFromInt(@intFromPtr(self) + 1);
             break :b r;
         };
     }
 
-    pub fn toSlice(self: *StackString) []u8 {
-        //return @constCast(@as([maxStackLen]u8, self.val)[0..self.len]);
+    inline fn toSlice(self: *StackString) []u8 {
         return b: {
             var r: []u8 = undefined;
             r.len = self.len;
-            // r.ptr = @ptrFromInt(@intFromPtr(&self.val));
             r.ptr = @ptrFromInt(@intFromPtr(self) + 1);
             break :b r;
         };
     }
 
-    pub fn initCopy(str: []const u8) StackString {
+    fn initCopy(str: []const u8) StackString {
         std.debug.assert(str.len <= maxStackLen);
         var r: StackString = .{};
         r.len = @truncate(str.len);
         const slice = r.toSlice();
-        @memset(slice, 0);
         @memcpy(slice, str);
-        r.len = @truncate(str.len);
         return r;
     }
 };
@@ -101,8 +93,10 @@ pub const String = packed union {
     }
 
     pub fn initCopy(str: []const u8) String {
-        if (str.len <= maxStackLen) {} else {
-            return String{ .heap = HeapString.initCopy(str) };
+        if (str.len <= maxStackLen) {
+            return String{ .stack = StackString.initCopy(str) };
+        } else {
+            return String{ .heap = HeapString.initCopy(str) catch |e| std.debug.panic("{any}{any}", .{ e, @errorReturnTrace() }) };
         }
     }
 
@@ -110,27 +104,42 @@ pub const String = packed union {
         if (self.isHeapString()) self.heap.deinit();
     }
 
-    pub fn toSlice(self: *String) []const u8 {
-        switch (self.isStackString()) {
-            .true => self.stack.toSlice(),
-            .false => self.heap.toSlice(),
-        }
+    pub fn toSlice(self: *String) []u8 {
+        if (self.isStackString()) return self.stack.toSlice();
+        return self.heap.toSlice();
+    }
+
+    pub fn toSliceC(self: *const String) []const u8 {
+        if (self.isStackString()) return self.stack.toSliceC();
+        return self.heap.toSliceC();
     }
 };
 
-test "Sizing" {
+test "Size and Alignment" {
     try std.testing.expectEqual(structSize, @sizeOf(StackString));
     try std.testing.expectEqual(structSize, @sizeOf(HeapString));
+    try std.testing.expectEqual(@alignOf(StackString), @alignOf(HeapString));
+    std.log.debug("{s} | size: {d}, align: {d}", .{ @typeName(StackString), @sizeOf(StackString), @alignOf(StackString) });
+    std.log.debug("{s} | size: {d}, align: {d}", .{ @typeName(HeapString), @sizeOf(HeapString), @alignOf(HeapString) });
 }
 
 test "Length" {
     var str: String = .{ .heap = .{} };
+    for (0..256) |i| {
+        str.stack.len = 0;
+        str.heap.len = i;
+        try std.testing.expectEqual(i, str.stack.len);
 
-    str.heap.len = 2;
-    try std.testing.expectEqual(2, str.stack.len);
+        str.heap.len = 0;
+        str.stack.len = @truncate(i);
+        try std.testing.expectEqual(i, str.heap.len);
+    }
 
-    str.stack.len = 3;
-    try std.testing.expectEqual(3, str.heap.len);
+    for (256..512) |i| {
+        str.stack.len = 0;
+        str.heap.len = i;
+        try std.testing.expect(str.heap.len != @as(@TypeOf(str.heap.len), @intCast(str.stack.len)));
+    }
 }
 
 test StackString {
@@ -150,4 +159,23 @@ test HeapString {
     defer str.deinit();
     try std.testing.expectEqualStrings(slcStr, str.toSliceC());
     try std.testing.expectEqualStrings(slcStr, str.toSlice());
+}
+
+test String {
+    var arrStr: [maxStackLen * 2]u8 = undefined;
+    const slcL: []u8 = arrStr[0..];
+    const slcS: []u8 = arrStr[0 .. maxStackLen - 1];
+    @memset(slcL, 'A');
+
+    var str_heap = String.initCopy(slcL);
+    defer str_heap.deinit();
+    try std.testing.expect(str_heap.isHeapString());
+    try std.testing.expectEqualStrings(slcL, str_heap.toSliceC());
+    try std.testing.expectEqualStrings(slcL, str_heap.toSlice());
+
+    var str_stack = String.initCopy(slcS);
+    defer str_heap.deinit();
+    try std.testing.expect(str_stack.isStackString());
+    try std.testing.expectEqualStrings(slcS, str_stack.toSliceC());
+    try std.testing.expectEqualStrings(slcS, str_stack.toSlice());
 }
